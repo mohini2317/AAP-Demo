06ee8711b8bd37b151b529ac999ece063f3abce3
Mohini Rahate <mohini.rahate@perficient.com>
2023-12-06 19:49:44 +0530

    update

--- Windows-Patching/roles/copy_script/tasks/CreateScheduledTask.ps1
+++ Windows-Patching/roles/copy_script/tasks/CreateScheduledTask.ps1
@@ -1,8 +1,23 @@
+# Define the number of days, hours, and minutes
+$days = 3
+$hours = 2
+$minutes = 30
+
+# Calculate the target date and time
+$targetDateTime = (Get-Date).AddDays($days).AddHours($hours).AddMinutes($minutes)
+
+# Format the target date and time as 'DD:HH:mm'
+$targetDateTimeFormatted = '{0:D2}:{1:D2}:{2:D2}' -f $targetDateTime.Days, $targetDateTime.Hours, $targetDateTime.Minutes
+
+# Define the trigger for the task (specific date and time)
+$trigger = New-ScheduledTaskTrigger -Once -At $targetDateTimeFormatted
+
+
 # Define parameters
 $TaskName = "MyScheduledTask"
 $ScriptPath = "C:\Users\Administrator\Desktop\Get-SystemStatus.ps1"
 $Action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-File $ScriptPath"
-$Trigger = New-ScheduledTaskTrigger -At "12:30" -Daily
+#$Trigger = New-ScheduledTaskTrigger -At "12:30" -Daily
 
 # Create the task
 Register-ScheduledTask -Action $Action -Trigger $Trigger -TaskName $TaskName -Description "Run My PowerShell Script"
